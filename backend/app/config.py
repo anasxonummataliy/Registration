539b2e028fee22091d70c200435f6ffc63b53341
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-04-06 00:13:33 +0500

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    db_url : str = "sqlite+aiosqlite///./auth.db"
+    db_url: str = "sqlite+aiosqlite:///./auth.db"
+
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from app.api.auth import router as auth_router
+from app.api.auth  import router as auth_router
 from app.database.base import Base 
 from app.database.session import async_engine
 import app.database.models.users
