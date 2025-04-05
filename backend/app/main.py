da050ef5e7f7e3f0243a121d5cad5131e249376c
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-04-05 11:36:08 +0500

    fix

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from app.api.auth import router as auth_router
+from app.database.base import Base 
+from app.database.session import async_engine
+import app.database.models.users
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metada.create_all)
+    yield
+    await async_engine.dispose()
+
+app = FastAPI(
+    title="Auth API",
+    description="Auth API",
+    version="1.0.0",
+    lifespan=lifespan,
+)
+
+
+app.include_router(auth_router)
